324c88f6dac313b8912f7c09d59e093f3c6966d1
Jerry Lott <jj@Jerrys-Mac-mini.local>
2022-11-18 18:32:36 -0600

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <h2 class="text-3xl text-green-700 font-extrabold">Hello Jerry</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+    
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
