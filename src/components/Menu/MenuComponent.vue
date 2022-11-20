3ed7b17af7a178faf75e3c22f832bf5acc307646
Jerry Lott <jj@Jerrys-Mac-mini.local>
2022-11-19 19:49:35 -0600

    Fixed Transition on Menu Component

--- src/components/Menu/MenuComponent.vue
+++ src/components/Menu/MenuComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <MenuView v-slot:default="slotProps" class="relative inline-block text-left">
     <MenuButton  class="inline-flex w-full justify-center rounded-md bg-blue-900 px-4 py-2 text-sm font-bold text-white hover:bg-opacity-70 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">Options</MenuButton>
-      <MenuItems v-if="slotProps.isOpen">
+      <MenuItems v-show="slotProps.isOpen">
         <MenuItem class="cursor-pointer text-blue-900 font-bold group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-900 hover:text-white">Account Settings</MenuItem>
         <MenuItem class="cursor-pointer text-blue-900 font-bold group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-900 hover:text-white">Support</MenuItem>
         <MenuItem class="cursor-pointer text-blue-900 font-bold group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-900 hover:text-white">License</MenuItem>
